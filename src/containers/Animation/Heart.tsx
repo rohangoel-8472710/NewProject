63de585cce3f7d27a986eb99808b19ed651d478c
rohangoel-847 <rohal.goel@appinventiv.com>
2020-05-08 20:05:07 +0530

    animation done for ios

--- src/containers/Animation/Heart.tsx
+++ src/containers/Animation/Heart.tsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import {Text, View, SafeAreaView} from 'react-native';
+import React, {Component} from 'react';
+import {SafeAreaView, View, Text, Image} from 'react-native';
 import LottieView from 'lottie-react-native';
 import styles from './styles';
+import images from '../../constants/images';
 
 export default class Heart extends React.Component {
-  animation: any;
-  componentDidMount() {
-    this.animation.play();
-  }
   render() {
     return (
-      <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <SafeAreaView style={styles.header}>
+          <Image style={styles.imageLines} source={images.threelines} />
+          <Text style={styles.headerText}>Animation</Text>
+        </SafeAreaView>
         <View style={styles.body}>
-          <Text>This is a Heart Animation</Text>
           <LottieView
-            ref={animation => {
-              this.animation = animation;
-            }}
             source={require('../../assets/heart.json')}
             style={styles.heartAnimation}
             autoPlay
             loop
           />
         </View>
-      </SafeAreaView>
+      </View>
     );
   }
 }
--- src/containers/Animation/styles.tsx
+++ src/containers/Animation/styles.tsx
@@ -1,5 +1,6 @@
 import {StyleSheet} from 'react-native';
 import {vw, vh} from '../../constants/dimensions';
+import colors from '../../constants/colors';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -13,6 +14,26 @@ const styles = StyleSheet.create({
   heartAnimation: {
     height: vw(400),
     width: vw(400),
+  },
+  header: {
+    flexDirection: 'row',
+    backgroundColor: colors.waterBlue,
+    alignItems: 'center',
+    width: vw(375),
+    height: vh(73),
+  },
+  imageLines: {
+    width: vw(18),
+    height: vh(16),
+    marginLeft: vw(12),
+    resizeMode: 'contain',
+    marginTop: vw(5),
+  },
+  headerText: {
+    fontSize: vw(21),
+    textAlign: 'center',
+    color: colors.textInput,
+    marginLeft: vw(120),
   },
 });
 export default styles;
